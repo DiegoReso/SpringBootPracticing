e93e0e044630eb753ff7654d820ab32a1a722fdf
Diego Reis <diegoreso@me.com>
2025-05-21 12:47:50 -0300

    controller created

--- src/main/java/dev/reso/practicing/controller/Controller.java
+++ src/main/java/dev/reso/practicing/controller/Controller.java
@@ -1,4 +1,18 @@
 package dev.reso.practicing.controller;
 
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/seeit")
 public class Controller {
+
+
+    @GetMapping
+    public String mostrarString(){
+        return "Oinn";
+    }
+
 }
