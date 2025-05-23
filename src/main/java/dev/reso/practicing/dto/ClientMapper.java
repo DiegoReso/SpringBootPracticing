946b32b16008753ec14662d9ff81184a074b64db
Diego Reis <diegoreso@me.com>
2025-05-23 17:53:08 -0300

    chore: creates client mapper and ClientDTO

--- src/main/java/dev/reso/practicing/dto/ClientMapper.java
+++ src/main/java/dev/reso/practicing/dto/ClientMapper.java
@@ -1,4 +1,32 @@
 package dev.reso.practicing.dto;
 
+import dev.reso.practicing.model.Client;
+import org.springframework.stereotype.Component;
+
+@Component
 public class ClientMapper {
+
+    public Client map(ClientDTO clientDTO){
+        Client client = new Client();
+        client.setId(clientDTO.getId());
+        client.setName(clientDTO.getName());
+        client.setAge(clientDTO.getAge());
+        client.setEmail(clientDTO.getEmail());
+        client.setRank(clientDTO.getRank());
+        client.setMissions(clientDTO.getMissions());
+
+        return client;
+    }
+
+    public ClientDTO map(Client client){
+        ClientDTO clientDTO = new ClientDTO();
+        clientDTO.setId(client.getId());
+        clientDTO.setName(client.getName());
+        clientDTO.setAge(client.getAge());
+        clientDTO.setEmail(client.getEmail());
+        clientDTO.setRank(client.getRank());
+        clientDTO.setMissions(client.getMissions());
+
+        return clientDTO;
+    }
 }
