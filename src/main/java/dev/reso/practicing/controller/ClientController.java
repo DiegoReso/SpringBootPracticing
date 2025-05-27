c6a079c0182c4c57af67117d5f2ac417aec70152
Diego Reis <diegoreso@me.com>
2025-05-27 10:31:07 -0300

    doc: adds swagger for api documentation

--- src/main/java/dev/reso/practicing/controller/ClientController.java
+++ src/main/java/dev/reso/practicing/controller/ClientController.java
@@ -2,6 +2,10 @@ package dev.reso.practicing.controller;
 
 import dev.reso.practicing.dto.ClientDTO;
 import dev.reso.practicing.service.ClientService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -18,28 +22,49 @@ public class ClientController {
     private ClientService clientService;
 
     @GetMapping
+    @Operation( summary = "GET ALL", description = "This route Finds All Clients")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Client Found"),
+            @ApiResponse(responseCode = "404", description = "Client Not Found")
+    })
     public ResponseEntity <List<ClientDTO>> getAll(){
         List<ClientDTO> list = clientService.getAll();
         return ResponseEntity.ok().body(list);
     }
 
+
     @GetMapping("/{id}")
+    @Operation(summary = "Gets Client By ID" , description = "This route Gets one client by id")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Client Found"),
+            @ApiResponse(responseCode = "404", description = "Client Not Found")
+    })
     public ResponseEntity<?> getById(@PathVariable Long id){
         ClientDTO client = clientService.getById(id);
 
         if(client != null){
-            return ResponseEntity.ok(client);
+            return ResponseEntity.ok().body(client);
         }else{
             return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Not Found");
         }
     }
 
+    @Operation(summary = "Inserts Client", description ="this route inserts the client")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Client Created"),
+            @ApiResponse(responseCode = "400", description = "Error creating client")
+    })
     @PostMapping
     public ResponseEntity<ClientDTO>  insertClient(@RequestBody ClientDTO client){
        ClientDTO clientDTO =  clientService.insert(client);
        return ResponseEntity.status(HttpStatus.CREATED).body(clientDTO);
     }
 
+    @Operation(summary = "Deletes Client by Id", description ="this route deletes the client by id")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "204", description = "Client Deleted"),
+            @ApiResponse(responseCode = "404", description = "Client Not Found")
+    })
     @DeleteMapping("/{id}")
     public ResponseEntity<String> deleteClient(@PathVariable Long id){
 
@@ -49,11 +74,19 @@ public class ClientController {
         }else{
             return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Ninja com id " + id + " nao encontrado!");
         }
-
     }
 
     @PutMapping("/{id}")
-    public ResponseEntity<?>  updateClient(@RequestBody ClientDTO client, @PathVariable Long id){
+    @Operation(summary = "Updates Client by ID", description = "This route updates the client by id")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Client Found"),
+            @ApiResponse(responseCode = "404", description = "Client Not Found")
+    })
+    public ResponseEntity<?>  updateClient(
+            @Parameter(description = "User sends the data for update Client")
+            @RequestBody ClientDTO client,
+            @Parameter(description = "User sends the id on path")
+            @PathVariable Long id){
         ClientDTO clientDTO =  clientService.update(client,id);
         if(clientDTO != null){
             return ResponseEntity.status(HttpStatus.CREATED).body(clientDTO);
